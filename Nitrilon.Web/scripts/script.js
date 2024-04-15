107e92b6f6dab1a89479a5b5805ae3694b679844
Patrick Sean Lange-Nielsen <39323@edu.campusvejle.dk>
2024-04-15 14:54:37 +0200

    added website, needs more work (js)

---
+++ Nitrilon.Web/scripts/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+let eventId = 1;
+let ratingId = 1;
+
+let greenPressed = document.querySelector("#green");
+let limePressed = document.querySelector("#lime");
+let yellowPressed = document.querySelector("#yellow");
+let orangePressed = document.querySelector("#orange");
+let redPressed = document.querySelector("#red");
+
+// Add eventlisteners to the smileys:
+greenPressed.addEventListener("click", sendToServer(1));
+limePressed.addEventListener("click", sendToServer(2));
+yellowPressed.addEventListener("click", sendToServer(3));
+orangePressed.addEventListener("click", sendToServer(4));
+redPressed.addEventListener("click", sendToServer(5));
+
+// Function to send the rating to the server:
+function sendToServer(rating)
+{
+    let apiURL = 'https://localhost:7268/api/EventRating?eventId${eventId}=1&ratingId=${ratingId}';
+    let requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    fetch(apiURL, requestOptions)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      }
+    )
+    .catch(error => {
+      console.error('Error: ', error);
+    })
+}
