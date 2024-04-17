0265dd0519bfcd4eebdd6315db5ff2e57382cbad
Patrick Sean Lange-Nielsen <39323@edu.campusvejle.dk>
2024-04-17 11:36:23 +0200

    added code to program.cs from mads' code

--- Nitrilon.Api/Program.cs
+++ Nitrilon.Api/Program.cs
@@ -7,6 +7,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+// Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:7123")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
